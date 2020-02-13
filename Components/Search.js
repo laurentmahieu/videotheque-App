b985e67d3a25137c6e48b9a4742e312e4737e1b4
Laurent <mahieu.l@hotmail.fr>
2020-02-13 21:40:05 +0100

    add style to Search n init a FlatList

--- Components/Search.js
+++ Components/Search.js
@@ -1,15 +1,33 @@
 import React from "react";
-import { View, TextInput, Button } from "react-native";
+import { StyleSheet, View, TextInput, Button, FlatList } from "react-native";
 
 class Search extends React.Component {
   render() {
     return (
-      <View>
-        <TextInput placeholder="Star Wars" />
+      <View style={{ marginTop: 20, backgroundColor: "red" }}>
+        <TextInput style={styles.textinput} placeholder="Titre du film" />
         <Button title="Rechercher" onPress={() => {}} />
+        <FlatList
+          data={[{ key: "a" }, { key: "b" }]}
+          renderItem={({ item }) => <Text>{item.key}</Text>}
+        />
       </View>
     );
   }
 }
+const styles = StyleSheet.create({
+  main_container: {
+    flex: 1,
+    marginTop: 20
+  },
+  textInput: {
+    marginLeft: 5,
+    marginRight: 5,
+    height: 50,
+    borderColor: "#000000",
+    borderWidth: 1,
+    paddingLeft: 5
+  }
+});
 
 export default Search;
