4aaccc862061d7690f7168853fb7d7ddb288cfcd
Laurent <mahieu.l@hotmail.fr>
2020-02-13 22:29:24 +0100

    update App

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
-import React from "react";
-import Search from "./Components/Search";
+// App.js
+
+import React from 'react'
+import Search from './Components/Search'
 
 export default class App extends React.Component {
   render() {
-    return <Search style={{ backgrouColor: "red" }} />;
+    return (
+      <Search/>
+    )
   }
 }
