55d3d2c64258819c42cd6b706985b175f8210628
Laurent <mahieu.l@hotmail.fr>
2020-02-15 09:48:06 +0100

    import fetch in search.js n log it

--- Components/Search.js
+++ Components/Search.js
@@ -11,13 +11,22 @@ import {
 } from "react-native";
 import films from "../Helpers/filmsData";
 import FilmItem from "./FilmItem";
+import { getFilmsFromApiWithSearchedText } from "../API/TMDBApi";
 
 class Search extends React.Component {
+  _loadFilms() {
+    getFilmsFromApiWithSearchedText("star").then(data => console.log(data));
+  }
   render() {
     return (
       <View style={styles.main_container}>
         <TextInput style={styles.textinput} placeholder="Titre du film" />
-        <Button title="Rechercher" onPress={() => {}} />
+        <Button
+          title="Rechercher"
+          onPress={() => {
+            this._loadFilms();
+          }}
+        />
         {/* Ici j'ai simplement repris l'exemple sur la documentation de la FlatList */}
         <FlatList
           data={films}
