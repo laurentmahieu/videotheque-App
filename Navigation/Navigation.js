1cfee51690263552a0c9e256ab6b58349c5d1c63
Laurent <mahieu.l@hotmail.fr>
2020-02-17 20:55:58 +0100

    init Navigation and show Search.js

---
+++ Navigation/Navigation.js
@@ -0,0 +1,13 @@
+// Navigation/Navigation.js
+
+import { createStackNavigator } from "react-navigation";
+import Search from "../Components/Search";
+
+const SearchStackNavigator = createStackNavigator({
+  Search: {
+    screen: Search,
+    navigationOptions: {
+      title: "Rechercher"
+    }
+  }
+});
