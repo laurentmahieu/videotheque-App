e0c61848f3b949168e24b699e715f47611eb304b
Laurent <mahieu.l@hotmail.fr>
2020-02-17 20:45:09 +0100

    update Search and TMDBApi

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -2,18 +2,19 @@
 
 const API_TOKEN = "e8d495fefb36495c5eef1030d785d66f";
 
-export function getFilmsFromApiWithSearchedText(text) {
+export function getFilmsFromApiWithSearchedText(text, page) {
   const url =
     "https://api.themoviedb.org/3/search/movie?api_key=" +
     API_TOKEN +
     "&language=fr&query=" +
-    text;
+    text +
+    "&page=" +
+    page;
   return fetch(url)
     .then(response => response.json())
     .catch(error => console.error(error));
 }
 
-
-export function getImageFromApi (name) {
-  return 'https://image.tmdb.org/t/p/w300' + name
+export function getImageFromApi(name) {
+  return "https://image.tmdb.org/t/p/w300" + name;
 }
