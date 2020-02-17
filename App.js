fb07eb4b11bfaf6aad61376247191c2f04758c44
Laurent <mahieu.l@hotmail.fr>
2020-02-17 22:34:38 +0100

    init react navigation in App.js

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
-// App.js
+// // App.js
+
+// import React from "react";
+// import Navigation from "./Navigation/Navigation";
+
+// export default class App extends React.Component {
+//   render() {
+//     return <Navigation />;
+//   }
+// }
+
+//_________________________________________________
 
 import React from "react";
-import Navigation from "./Navigation/Navigation";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default class App extends React.Component {
-  render() {
-    return <Navigation />;
-  }
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
 }
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
