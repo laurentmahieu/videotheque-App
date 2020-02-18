19f1b5eb03f7366e0dd2d7f67e2db74d07aed8be
Laurent <mahieu.l@hotmail.fr>
2020-02-18 09:40:50 +0100

    first View transform to TouchableOpacity to be able to onPress n lauch function

--- Components/FilmItem.js
+++ Components/FilmItem.js
@@ -1,14 +1,19 @@
 // Components/FilmItem.js
 
 import React from "react";
-import { StyleSheet, View, Text, Image } from "react-native";
+import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
 import { getImageFromApi } from "../API/TMDBApi";
 
 class FilmItem extends React.Component {
   render() {
     const { film, displayDetailForFilm } = this.props;
     return (
-      <View style={styles.main_container}>
+      <TouchableOpacity
+        style={styles.main_container}
+        onPress={() => {
+          displayDetailForFilm(film.id);
+        }}
+      >
         <Image
           style={styles.image}
           source={{ uri: getImageFromApi(film.poster_path) }}
@@ -27,7 +32,7 @@ class FilmItem extends React.Component {
             <Text style={styles.date_text}>Sorti le {film.release_date}</Text>
           </View>
         </View>
-      </View>
+      </TouchableOpacity>
     );
   }
 }
