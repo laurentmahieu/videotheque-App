93478f593d0e7b5ebf3d0e339b608f821fbf99b7
Laurent <mahieu.l@hotmail.fr>
2020-02-18 21:36:42 +0100

    add store in App

--- App.js
+++ App.js
@@ -2,9 +2,16 @@
 
 import React from "react";
 import Navigation from "./Navigation/Navigation";
+import { Provider } from "react-redux";
+import Store from "./Store/configureStore";
 
-function App() {
-  return <Navigation />;
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={Store}>
+        <Navigation />
+      </Provider>
+    );
+  }
 }
 
-export default App;
