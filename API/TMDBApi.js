5217b11cd360e41b98e71151358833db53fc6849
Laurent <mahieu.l@hotmail.fr>
2020-02-18 13:09:49 +0100

    add function to get film detail in TMDBApi.js

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -2,6 +2,7 @@
 
 const API_TOKEN = "e8d495fefb36495c5eef1030d785d66f";
 
+// Récupération des films
 export function getFilmsFromApiWithSearchedText(text, page) {
   const url =
     "https://api.themoviedb.org/3/search/movie?api_key=" +
@@ -15,6 +16,20 @@ export function getFilmsFromApiWithSearchedText(text, page) {
     .catch(error => console.error(error));
 }
 
+// Récupération de l'image d'un film
 export function getImageFromApi(name) {
   return "https://image.tmdb.org/t/p/w300" + name;
+}
+
+// Récupération du détail d'un film
+export function getFilmDetailFromApi(id) {
+  return fetch(
+    "https://api.themoviedb.org/3/movie/" +
+      id +
+      "?api_key=" +
+      API_TOKEN +
+      "&language=fr"
+  )
+    .then(response => response.json())
+    .catch(error => console.error(error));
 }
