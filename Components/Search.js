ff36f990d970cb8ddb29f566c2b2816eafd37fb5
Laurent <mahieu.l@hotmail.fr>
2020-02-18 09:21:18 +0100

    init function to get movie infos in search.js

--- Components/Search.js
+++ Components/Search.js
@@ -25,6 +25,10 @@ class Search extends React.Component {
     };
   }
 
+  _displayDetailForFilm = idFilm => {
+    console.log("Display film with id" + idFilm);
+  };
+
   _loadFilms() {
     if (this.searchedText.length > 0) {
       this.setState({ isLoading: true });
@@ -81,7 +85,12 @@ class Search extends React.Component {
         <FlatList
           data={this.state.films}
           keyExtractor={item => item.id.toString()}
-          renderItem={({ item }) => <FilmItem film={item} />}
+          renderItem={({ item }) => (
+            <FilmItem
+              film={item}
+              _displayDetailForFilm={this._displayDetailForFilm}
+            />
+          )}
           onEndReachedThreshold={0.5}
           onEndReached={() => {
             if (this.page < this.totalPages) {
