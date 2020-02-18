ec7a2e99c9762d3dc779d1557b62dd80b5db97ba
Laurent <mahieu.l@hotmail.fr>
2020-02-18 13:31:27 +0100

    init the loading function

--- Components/FilmDetail.js
+++ Components/FilmDetail.js
@@ -1,14 +1,30 @@
 // Components/FilmDetail.js
 
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, ActivityIndicator } from "react-native";
 
 class FilmDetail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      film: undefined,
+      isLoading: true
+    };
+  }
+
+  _displayLoading() {
+    if (this.state.Loading) {
+      return (
+        <View style={styles.loading_container}>
+          <ActivityIndicator size="large" />
+        </View>
+      );
+    }
+  }
+
   render() {
     return (
-      <View style={styles.main_container}>
-        <Text>Détail du film {this.props.route.params.idFilm}</Text>
-      </View>
+      <View style={styles.main_container}>{() => this._displayLoading()}</View>
     );
   }
 }
@@ -16,6 +32,15 @@ class FilmDetail extends React.Component {
 const styles = StyleSheet.create({
   main_container: {
     flex: 1
+  },
+  loading_container: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: "center",
+    justifyContent: "center"
   }
 });
 
