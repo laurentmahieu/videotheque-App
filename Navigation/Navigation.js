683d6fa361ee70971c1de80372570f223da4a5ce
Laurent <mahieu.l@hotmail.fr>
2020-02-19 22:10:53 +0100

    restore Stack.Navigator in Navigation.js

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -2,29 +2,34 @@
 
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-// import { createStackNavigator } from "@react-navigation/stack";
-import Search from "../Components/Search";
-// import FilmDetail from "../Components/FilmDetail";
-import Favorites from "../Components/Favorites";
+import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Favorites from "../Components/Favorites";
+import Search from "../Components/Search";
+import FilmDetail from "../Components/FilmDetail";
 
-// const Stack = createStackNavigator();
+const SearchStack = createStackNavigator();
+
+function SearchStackScreen() {
+  return (
+    <SearchStack.Navigator>
+      <SearchStack.Screen name="Rechercher" component={Search} />
+      <SearchStack.Screen name="FilmDetail" component={FilmDetail} />
+    </SearchStack.Navigator>
+  );
+}
+
+const BottomTab = createBottomTabNavigator();
 
 function Navigation() {
   return (
     <NavigationContainer>
-      {/* <Stack.Navigator>
-        <Stack.Screen name="Rechercher" component={Search} />
-        <Stack.Screen name="FilmDetail" component={FilmDetail} />
-      </Stack.Navigator> */}
-      <Tab.Navigator>
-        <Tab.Screen name="Search" component={Search} />
-        <Tab.Screen name="Favorites" component={Favorites} />
-      </Tab.Navigator>
+      <BottomTab.Navigator>
+        <BottomTab.Screen name="Search" component={SearchStackScreen} />
+        <BottomTab.Screen name="Favorites" component={Favorites} />
+      </BottomTab.Navigator>
     </NavigationContainer>
   );
 }
-
-const Tab = createBottomTabNavigator();
 
 export default Navigation;
