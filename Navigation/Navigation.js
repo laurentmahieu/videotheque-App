2f2ee30267b92ae9b9f8497645d8bde1cf17b60a
Laurent <mahieu.l@hotmail.fr>
2020-02-19 21:29:19 +0100

    init Navigation with bottomTabNavigator

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -2,21 +2,29 @@
 
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+// import { createStackNavigator } from "@react-navigation/stack";
 import Search from "../Components/Search";
-import FilmDetail from "../Components/FilmDetail";
+// import FilmDetail from "../Components/FilmDetail";
+import Favorites from "../Components/Favorites";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-const Stack = createStackNavigator();
+// const Stack = createStackNavigator();
 
 function Navigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      {/* <Stack.Navigator>
         <Stack.Screen name="Rechercher" component={Search} />
         <Stack.Screen name="FilmDetail" component={FilmDetail} />
-      </Stack.Navigator>
+      </Stack.Navigator> */}
+      <Tab.Navigator>
+        <Tab.Screen name="Search" component={Search} />
+        <Tab.Screen name="Favorites" component={Favorites} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
+
+const Tab = createBottomTabNavigator();
 
 export default Navigation;
