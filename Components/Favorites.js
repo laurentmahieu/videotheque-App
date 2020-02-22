51b64f75eda178df2add6cd2ac31caab157b6393
Laurent <mahieu.l@hotmail.fr>
2020-02-22 08:56:04 +0100

    import FilmList in Favorites with booléen true to stop searching films on srcoll in Favorites.js

--- Components/Favorites.js
+++ Components/Favorites.js
@@ -3,18 +3,25 @@
 import React from "react";
 import { StyleSheet, Text } from "react-native";
 import { connect } from "react-redux";
+import FilmList from "../Components/FilmList";
 
 class Favorites extends React.Component {
   render() {
-    return <Text>Mes Favoris</Text>;
+    return (
+      <FilmList
+        films={this.props.favoritesFilm}
+        navigation={this.props.navigation}
+        FavoriteList={true} // ici on est bien dans le cas de la liste des films favoris. Ce booléen à true permettra d'empêcher de lancer la recherche de plus de films après le scroll lorsquon est sur la vue Favoris.
+      />
+    );
   }
 }
 
 const styles = StyleSheet.create({});
 
 const mapStateToProps = state => {
-  return { 
-    favoritesFilm: state.favoritesFilm 
+  return {
+    favoritesFilm: state.favoritesFilm
   };
 };
 
