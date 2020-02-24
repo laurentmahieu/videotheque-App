9d4e4e1dff8d95787747d46facb6727906678fa7
Laurent <mahieu.l@hotmail.fr>
2020-02-24 12:02:24 +0100

    delete component Home from Search.js

--- Components/Search.js
+++ Components/Search.js
@@ -4,7 +4,6 @@ import React from "react";
 import { StyleSheet, View, TextInput, ActivityIndicator } from "react-native";
 import { getFilmsFromApiWithSearchedText } from "../API/TMDBApi";
 import FilmList from "./FilmList";
-import Home from "./Home";
 
 class Search extends React.Component {
   constructor(props) {
@@ -71,18 +70,14 @@ class Search extends React.Component {
           onChangeText={text => this._searchTextInputChanged(text)}
           onSubmitEditing={() => this._searchFilms()}
         />
-        {this.totalPages === 0 ? (
-          <Home />
-        ) : (
-          <FilmList
-            films={this.state.films} // c'est bien Search qui récupère les donées depuis l'API et on les transmet ici pour que le component FilmList les affiche
-            navigation={this.props.navigation} //ici on transmet les informations de navigation pour permettre au component FilmList de naviguer vers le détails d'un film
-            loadFilms={this._loadFilms} //_loadFilms charge les films suivants, ça concerne l'API, le component FilmList va juste appeler cette méthode quand l'utilisateur aura parcouru tous les films et c'est le component Search qui lui fournira les films suivants
-            page={this.page}
-            totalPages={this.totalPages} // les infos page et totalPages vont être utile, coté component FilmList pour ne pas déclencher l'évènement pour ch  arger plus de film si on a atteint la dernière page
-            favoritesList={false} //ic j'ai s'implement ajouté un booléen à false pour indiquer qu'on n'est pas dans le cas de l'affichage de la liste des films favoris. Et ainsi pouvoir déclancher le chargement de plus de films lorsque l'utilisateur scrolle.
-          />
-        )}
+        <FilmList
+          films={this.state.films} // c'est bien Search qui récupère les donées depuis l'API et on les transmet ici pour que le component FilmList les affiche
+          navigation={this.props.navigation} //ici on transmet les informations de navigation pour permettre au component FilmList de naviguer vers le détails d'un film
+          loadFilms={this._loadFilms} //_loadFilms charge les films suivants, ça concerne l'API, le component FilmList va juste appeler cette méthode quand l'utilisateur aura parcouru tous les films et c'est le component Search qui lui fournira les films suivants
+          page={this.page}
+          totalPages={this.totalPages} // les infos page et totalPages vont être utile, coté component FilmList pour ne pas déclencher l'évènement pour ch  arger plus de film si on a atteint la dernière page
+          favoritesList={false} //ic j'ai s'implement ajouté un booléen à false pour indiquer qu'on n'est pas dans le cas de l'affichage de la liste des films favoris. Et ainsi pouvoir déclancher le chargement de plus de films lorsque l'utilisateur scrolle.
+        />
         {this._displayLoading()}
       </View>
     );
